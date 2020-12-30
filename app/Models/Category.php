40b938b2cdfe997c32496d3722eafaf0d660bdda
wael al halabi <wael120120@gmail.com>
2020-12-30 23:20:06 +0200

    Category API

---
+++ app/Models/Category.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+
+
+/**
+ * @SWG\Definition(
+ *      definition="Category",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="name",
+ *          description="name",
+ *          type="integer",
+ *          format="int32"
+ *      )
+ * )
+ */
+class Category extends Model
+{
+
+
+    public $table = 'categories';
+    
+
+
+
+    public $fillable = [
+        'name'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'integer'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'equired|max:50|unique:categories'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     **/
+    public function articles()
+    {
+        return $this->hasMany(\App\Models\Article::class);
+    }
+}
---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class CategoryRepository
+ * @package App\Repositories
+ * @version December 30, 2020, 9:14 pm UTC
+*/
+
+class CategoryRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Category::class;
+    }
+}
